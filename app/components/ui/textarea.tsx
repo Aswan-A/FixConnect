dbef1816f91590355272f92fd5b750022707923c
Noah12398 <149400947+Noah12398@users.noreply.github.com>
2025-10-17 11:35:57 +0530

    UI Change

--- app/components/ui/textarea.tsx
+++ app/components/ui/textarea.tsx
@@ -7,7 +7,7 @@ function Textarea({ className, ...props }: React.ComponentProps<"textarea">) {
     <textarea
       data-slot="textarea"
       className={cn(
-"border-0 placeholder:text-muted-foreground  flex field-sizing-content min-h-16 w-full rounded-md bg-transparent px-3 py-2 text-base outline-none  disabled:cursor-not-allowed disabled:opacity-50 md:text-sm"
+"border-0 placeholder:text-muted-foreground dark:bg-input/30 flex field-sizing-content min-h-16 w-full rounded-md bg-transparent px-3 py-2 text-base outline-none  disabled:cursor-not-allowed disabled:opacity-50 md:text-sm"
 ,        className
       )}
       {...props}
--- app/routes/_layout.createissue.tsx
+++ app/routes/_layout.createissue.tsx
@@ -162,7 +162,7 @@ export default function CreateIssue() {
             <CardFooter className="pt-4">
               <Button
                 type="submit"
-                className="w-full bg-green-600 hover:bg-green-700 text-white"
+                className="w-full text-white"
                 disabled={isSubmitting}
               >
                 {isSubmitting ? "Submitting..." : "Create Issue"}
